b89836ccb509a5752cee0ce18e7e682dbdff1a12
Lucas Ribeiro <lucaorpacheco@gmail.com>
2024-06-16 13:24:04 -0300

    + dependencias do springSecurity para que funcione a autenticação do projeto.

---
+++ src/main/java/br/com/api/biju/gestao_loja/modules/cliente/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package br.com.api.biju.gestao_loja.modules.cliente.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/produto/create").permitAll()
+                            .requestMatchers("/cliente").permitAll()
+                            .requestMatchers("/pecacorda").permitAll()
+                            .requestMatchers("/pecapingente/create").permitAll();
+                    auth.anyRequest().authenticated();
+                });
+        return http.build();
+    }
+}
